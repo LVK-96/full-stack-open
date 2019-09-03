c6e686dd1e33d6629d76220e5bc42d1ea3d3a534
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-09-03 19:55:48 +0300

    7.16 done

--- part7/bloglist-frontend/src/index.js
+++ part7/bloglist-frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
--- part7/bloglist-frontend/src/services/users.js
+++ part7/bloglist-frontend/src/services/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = '/api/users';
+const baseUrl = `${BACKEND_URL}/api/users`;
 
 let token = null;
 
