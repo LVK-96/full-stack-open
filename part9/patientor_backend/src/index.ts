98e155a722bd4900e77d8999ba29aa1a3fcb7195
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-04-11 16:12:23 +0300

    9.10 done

--- part9/patientor_backend/src/index.ts
+++ part9/patientor_backend/src/index.ts
@@ -1,16 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/diagnoses', diagnoseRouter);
+
 const PORT = 3001;
-
-app.get('/ping', (_req, res) => {
-  res.send('pong');
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on: http://localhost:${PORT}`);
 });
